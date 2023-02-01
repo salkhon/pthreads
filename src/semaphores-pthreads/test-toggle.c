606541401a8c7a5cd57b78ef0a1113f3533c5a38
alfahadarc <alfahadarc@gmail.com>
2023-02-01 22:03:54 +0600

    zem done

--- src/semaphores-pthreads/test-toggle.c
+++ src/semaphores-pthreads/test-toggle.c
@@ -6,9 +6,13 @@
 #include <signal.h>
 #include <wait.h>
 #include <pthread.h>
+#include "zemaphore.h"
 
 #define NUM_THREADS 3
 #define NUM_ITER 10
+
+zem_t zem[NUM_THREADS];
+
 
 void *justprint(void *data)
 {
@@ -16,7 +20,9 @@ void *justprint(void *data)
 
   for(int i=0; i < NUM_ITER; i++)
     {
+      zem_down(&zem[thread_id]);
       printf("This is thread %d\n", thread_id);
+      zem_up(&zem[(thread_id +1 ) % NUM_THREADS]);
     }
   return 0;
 }
@@ -26,6 +32,10 @@ int main(int argc, char *argv[])
 
   pthread_t mythreads[NUM_THREADS];
   int mythread_id[NUM_THREADS];
+
+    zem_init(&zem[0], 1);
+    zem_init(&zem[1], 0);
+    zem_init(&zem[2], 0);
 
   
   for(int i =0; i < NUM_THREADS; i++)
