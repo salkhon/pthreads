8b280420c62ae5703b551d713daa4b3015bb448c
alfahadarc <alfahadarc@gmail.com>
2023-02-05 23:48:02 +0600

    done IG

--- src/new-offline/bonding.cpp
+++ src/new-offline/bonding.cpp
@@ -18,68 +18,57 @@ pthread_cond_t hydro_cond;
 
 void bond()
 {
-    printf("bondings  H2O \r\n");
     count++;
+    printf("bondings  H2O %d \r\n", count);
+    
 }
 
 void *oxygene(void *args)
 {
     pthread_mutex_lock(&lock);
     oxy++;
-    while (1)
-    {
-        if (hydro >= 2)
-        {
-            bond();
-            hydro = hydro - 2;
-            oxy--;
-            break;
-        }
-        else
-        {
-            if (hydro >= 1)
-            {
-                pthread_cond_signal(&hydro_cond);
-            }
-            else
-            {
-                pthread_cond_wait(&oxy_cond, &lock);
-            }
-        }
-    }
-    pthread_mutex_unlock(&lock);
 
+    if (hydro >= 2 && oxy >= 1)
+    {
+        bond();
+        hydro = hydro - 2;
+        oxy--;
+        pthread_cond_signal(&oxy_cond);
+        pthread_cond_signal(&hydro_cond);
+        pthread_cond_signal(&hydro_cond);
+    }
+    else
+    {
+        pthread_cond_wait(&oxy_cond, &lock);
+    }
+
+    pthread_mutex_unlock(&lock);
+    pthread_exit(0);
 }
 
 void *hydrogen(void *args)
 {
     pthread_mutex_lock(&lock);
     hydro++;
-    while (1)
-    {
-        if (hydro >= 2 && oxy >= 1)
-        {
-            bond();
-            hydro = hydro - 2;
-            oxy--;
-            break;
-        }
-        else
-        {
-            if (hydro >= 2)
-            {
-                pthread_cond_signal(&oxy_cond);
-            }
-            else
-            {
-                pthread_cond_wait(&hydro_cond, &lock);
-            }
-        }
-    }
-    pthread_mutex_unlock(&lock);
-    return NULL;
-}
 
+    if (hydro >= 2 && oxy >= 1)
+    {
+        bond();
+        hydro = hydro - 2;
+        oxy--;
+        pthread_cond_signal(&oxy_cond);
+        pthread_cond_signal(&hydro_cond);
+        pthread_cond_signal(&hydro_cond);
+    }
+    else
+    {
+
+        pthread_cond_wait(&hydro_cond, &lock);
+    }
+
+    pthread_mutex_unlock(&lock);
+    pthread_exit(0);
+}
 
 int main()
 {
@@ -88,16 +77,17 @@ int main()
     pthread_cond_init(&hydro_cond, NULL);
     printf("Starting..\r\n");
 
-    for (int k = 0; k < 3; k++)
+    for (int k = 0; k < 10; k++)
     {
         if (pthread_create(&thread_oxy, NULL, oxygene, NULL))
             cout << "Error while creating thread" << endl;
     }
-    for (int k = 0; k < 3; k++)
+    for (int k = 0; k < 20; k++)
     {
         if (pthread_create(&thread_hydro, NULL, hydrogen, NULL))
             cout << "Error while creating thread" << endl;
     }
     pthread_join(thread_oxy, NULL);
     pthread_join(thread_hydro, NULL);
+    return 0;
 }
