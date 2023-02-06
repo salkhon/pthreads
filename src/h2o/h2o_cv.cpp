1450099a763ca57430a65f55511a9b3e9cd65940
salkhon <salmankhondker1998@gmail.com>
2023-02-06 19:26:47 +0600

    done

---
+++ src/h2o/h2o_cv.cpp
@@ -0,0 +1,84 @@
+#include "../semaphores-pthreads/zemaphore.h"
+#include <iostream>
+#include <stdio.h>
+#include <pthread.h>
+
+pthread_t thread_oxy;
+pthread_t thread_hydro;
+
+pthread_mutex_t lock;
+int oxy, hydro;
+int count = 0;
+
+pthread_cond_t oxy_cond;
+pthread_cond_t hydro_cond;
+
+void bond() {
+    count++;
+    std::cout << "bonding H2O " << count << std::endl;
+}
+
+void* oxygene(void* args) {
+    pthread_mutex_lock(&lock);
+    oxy++;
+
+    if (hydro >= 2 && oxy >= 1) {
+        bond();
+        hydro = hydro - 2;
+        oxy--;
+        pthread_cond_signal(&oxy_cond);
+        pthread_cond_signal(&hydro_cond);
+        pthread_cond_signal(&hydro_cond);
+    } else {
+        pthread_cond_wait(&oxy_cond, &lock);
+    }
+
+    pthread_mutex_unlock(&lock);
+    pthread_exit(0);
+}
+
+void* hydrogen(void* args) {
+    pthread_mutex_lock(&lock);
+    hydro++;
+
+    if (hydro >= 2 && oxy >= 1) {
+        bond();
+        hydro = hydro - 2;
+        oxy--;
+        pthread_cond_signal(&oxy_cond);
+        pthread_cond_signal(&hydro_cond);
+        pthread_cond_signal(&hydro_cond);
+    } else {
+
+        pthread_cond_wait(&hydro_cond, &lock);
+    }
+
+    pthread_mutex_unlock(&lock);
+    pthread_exit(0);
+}
+
+int main(int argc, char* argv[]) {
+    int num_o = std::atoi(argv[1]);
+
+    pthread_mutex_init(&lock, NULL);
+    pthread_cond_init(&oxy_cond, NULL);
+    pthread_cond_init(&hydro_cond, NULL);
+    printf("Starting..\r\n");
+
+    for (int k = 0; k < num_o; k++) {
+        if (pthread_create(&thread_oxy, NULL, oxygene, NULL)) {
+            std::cout << "Error while creating thread" << std::endl;
+        }
+    }
+
+    for (int k = 0; k < 2 * num_o; k++) {
+        if (pthread_create(&thread_hydro, NULL, hydrogen, NULL)) {
+            std::cout << "Error while creating thread" << std::endl;
+        }
+    }
+
+    pthread_join(thread_oxy, NULL);
+    pthread_join(thread_hydro, NULL);
+
+    return 0;
+}
---
+++ src/h2o/h2o_sem.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <semaphore.h>
+#include "../semaphores-pthreads/zemaphore.h"
+
+zem_t counter_sem, oxy_sem, hydro_sem, bond_sem;
+int nhydro = 0, nmol = 0;
+
+int count = 0;
+void bond(std::string atom, int thread_id) {
+    if ((count % 3 == 2 && atom != "Oxygen") || (count % 3 != 2 && atom != "Hydrogen")) {
+        std::cerr << "ERROR " << count << std::endl;
+    }
+
+    std::cout << atom << " thread " << thread_id << " bonding for molecule " << nmol << std::endl;
+    count++;
+}
+
+// let only oxygen post all sems, when bonding is done
+// sem that allows 2 h, 1 o. when 3 of them are done, only o posts all sems.
+// this allows another 2 h 1 o to pass. 
+
+void* hydro_worker(void* args) {
+    long long thread_id = (long long)args;
+
+    zem_down(&hydro_sem); // 2 h can pass until 1 o passes
+
+    zem_down(&counter_sem);
+    nhydro++; // to only make the second hydrogen wake the oxygen
+    bond("Hydrogen", thread_id); // to make printing atomic
+    zem_up(&counter_sem);
+
+    if (nhydro == 2) {
+        zem_up(&bond_sem); // 2nd h signals o that it can bond
+    }
+
+    return NULL;
+}
+
+void* oxy_worker(void* args) {
+    long long thread_id = (long long)args;
+
+    zem_down(&oxy_sem); // only 1 o can pass
+
+    zem_down(&bond_sem); // waiting for the 2nd h to pass
+
+    zem_down(&counter_sem);
+    bond("Oxygen", thread_id);
+    nhydro -= 2;
+    zem_up(&counter_sem);
+
+    nmol++;
+
+    zem_up(&oxy_sem);
+    zem_up(&hydro_sem);
+    zem_up(&hydro_sem);
+
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Expected argument count: 2, but got " << argc << ". Pass in molecule count." << std::endl;
+        exit(1);
+    }
+
+    int num_o = std::atoi(argv[1]);
+
+    zem_init(&counter_sem, 1); // used as mutex
+    zem_init(&oxy_sem, 1);
+    zem_init(&hydro_sem, 2);
+    zem_init(&bond_sem, 0);
+
+    pthread_t* oxythreads = (pthread_t*)malloc(num_o * sizeof(pthread_t));
+    pthread_t* hydrothreads = (pthread_t*)malloc(2 * num_o * sizeof(pthread_t));
+
+    for (size_t i = 0; i < num_o; i++) {
+        pthread_create(&oxythreads[i], NULL, oxy_worker, (void*)i);
+        pthread_create(&hydrothreads[i], NULL, hydro_worker, (void*)i);
+        pthread_create(&hydrothreads[num_o + i], NULL, hydro_worker, (void*)(num_o + i));
+    }
+
+    for (size_t i = 0; i < num_o; i++) {
+        pthread_join(oxythreads[i], NULL);
+        pthread_join(hydrothreads[i], NULL);
+        pthread_join(hydrothreads[num_o + i], NULL);
+    }
+
+    return 0;
+}
